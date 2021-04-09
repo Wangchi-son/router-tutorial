02ae4655c38f83c9645f12e1ce58b6a4f017d278
unknown <sonwj0915@naver.com>
2021-04-09 23:01:22 +0900

    서브라우트

--- src/About.js
+++ src/About.js
@@ -12,6 +12,7 @@ function About({ location }) {
     <div>
       <h1>소개</h1>
       <p>이 프로젝트는 리액트 라우터 기초를 실습해보는 예제 프로젝트입니다.</p>
+      {/* http://localhost:3000/about?detail=true */}
       {detail && <p>detail값이 true입니다</p>}
     </div>
   );
